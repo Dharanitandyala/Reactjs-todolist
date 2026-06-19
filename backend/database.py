8a1d022f156c60b09c1e1a04b9b0b264360de949
Dharani Tandyala <dharanitandyala2@gmail.com>
2026-06-19 09:08:55 +0530

    reset database

--- backend/database.py
+++ backend/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DATABASE_URL = "sqlite:///./todos.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
 
 engine = create_engine(
-    DATABASE_URL,
+    SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False}
 )
 
